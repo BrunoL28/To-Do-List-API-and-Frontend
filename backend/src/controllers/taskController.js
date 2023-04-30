6628979173ac35ab0f2130ffa1e26a99f72e0612
Bruno Lima <brunolima280103@gmail.com>
2023-04-30 03:55:16 -0300

    Controllers : Passando função que cria nova task.

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,12 +1,21 @@
 const taskModel = require( "../models/taskModel" );
 
-const getAll = async ( request, response ) => {
+const getAll = async ( _request, response ) => {
 
-    const task = await taskModel.getAll();
+    const tasks = await taskModel.getAll();
 
-    return response.status(200).json(task);
+    return response.status(200).json(tasks);
+};
+
+const newTask = async ( request, response ) => {
+
+    const createdTask = await taskModel.newTask( request.body );
+
+    return response.status(201).json(createdTask);
+
 };
 
 module.exports = {
-    getAll
+    getAll,
+    newTask
 };
