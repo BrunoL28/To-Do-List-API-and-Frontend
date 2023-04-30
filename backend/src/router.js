6db9110dfb642b43d5f28f341ec2c75e30ce67ac
Bruno Lima <brunolima280103@gmail.com>
2023-04-30 02:28:15 -0300

    Implementada a rota get.

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,9 @@
 const express = require( "express" );
 
+const taskController = require( "./controllers/taskController" );
+
 const router = express.Router();
+
+router.get( "/tasks", taskController.getAll);
 
 module.exports = router;
