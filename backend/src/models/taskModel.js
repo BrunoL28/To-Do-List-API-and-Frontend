0f3dfc0f4decb1f950372f858c8c3833b3377ace
Bruno Lima <brunolima280103@gmail.com>
2023-05-01 17:47:45 -0300

    Implementando funções para as rotas delete e put.

--- backend/src/models/taskModel.js
+++ backend/src/models/taskModel.js
@@ -1,7 +1,7 @@
-const connection = require( "./connection" );
+const connection = require('./connection');
 
 const getAll = async () => {
-    const [tasks] = await connection.execute( "SELECT * FROM tasks" );
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
     return tasks;
 };
 
@@ -9,16 +9,36 @@ const newTask = async (task) => {
     
     const { title } = task;
 
-    const query = "INSERT INTO tasks( title, status, created_at ) VALUES (?, ?, ?)";
-
     const dateUTC = new Date(Date.now()).toUTCString;
 
-    const [createdTask] = await connection.execute(query, [title, "pendente", dateUTC]);
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
+
+    const [createdTask] = await connection.execute(query, [title, 'pendente', dateUTC]);
 
     return {insertId: createdTask.insertId};
 };
 
+const deleteTask = async (id) => {
+    
+    const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+
+    return removedTask;
+};
+
+const updateTask = async (id, task) => {
+
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?';
+
+    const { title, status } = task;
+
+    const [updatedTask] = await connection.execute(query, [title, status, id]);
+
+    return updatedTask;
+};
+
 module.exports = {
     getAll,
-    newTask
+    newTask,
+    deleteTask,
+    updateTask,
 };
