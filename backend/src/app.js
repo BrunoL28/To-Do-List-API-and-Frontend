e61f0c839050f14aa007f6afe80a5bc0b8b67ff6
Bruno Lima <brunolima280103@gmail.com>
2023-05-02 00:54:06 -0300

    Acrescentando body-parser e cors no projeto.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const router = require( './router' );
+const router = require('./router');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(router);
 
 app.use(express.json());
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+app.use(cors());
 
 module.exports = app;
