36fe12a0522ba0561d9a9139fe8a303612f23a81
Bruno Lima <brunolima280103@gmail.com>
2023-05-03 00:32:37 -0300

    Script: Funções para implementar elementos presentes no html.

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,5 @@
+const tbody = document.querySelector('tbody');
+
 const fetchTasks = async () => {
 
     const response = await fetch('http://localhost:3333/tasks');
@@ -5,4 +7,56 @@ const fetchTasks = async () => {
     const tasks = await response.json();
 
     return tasks;
+};
+
+const createElement = (tag, innerText = '', innerHTML = '') => {
+
+    const element = document.createElement(tag);
+
+    if ( innerText ) {
+        element.innerText = innerText;
+    }
+
+    if ( innerHTML ) {
+        element.innerHTML = innerHTML;
+    }
+
+    return element;
+};
+
+const createSelect = () => {
+    
+    const options = '<option value="pendente">Pendente</option><option value="em andamento">Em Andamento</option><option value="concluida">Concluída</option>';
+
+    const select = createElement('select', '', options);
+
+    return select;
+};
+
+const createRow = (task) => {
+
+    const { id, title, created_at, status } = task;
+
+    const tr = createElement('tr');
+    const tdTitle = createElement('td', title);
+    const tdCreatedAt = createElement('td', created_at);
+    const tdStatus = createElement('td');
+    const tdActions = createElement('td');
+
+    const editButton = createElement('button', '', '<span class="material-symbols-outlined">edit</span>');
+    const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">delete</span>');
+
+    editButton.classList.add('btn-action');
+    deleteButton.classList.add('btn-action');
+
+    tdActions.appendChild(editButton);
+    tdActions.appendChild(deleteButton);
+
+    tr.appendChild(tdTitle);
+    tr.appendChild(tdCreatedAt);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdActions);
+
+    tbody.appendChild(tr);
+
 };
